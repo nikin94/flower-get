15d22bcdc19d39d2ab1a72f5ea14ef0ed5d589c0
nikin94 <z.94@bk.ru>
2018-03-19 17:26:09 +0300

    added css, bg

--- index.php
+++ index.php
@@ -10,10 +10,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Цветы</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-hello world!
+<header>
+    <nav>
+        <ul>
+            <li><a href="">Список заказов</a></li>
+            <li><a href="">Добавить новый заказ</a></li>
+        </ul>
+    </nav>
+</header>
+
 <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="assets/js/script.js"></script>
 </body>
